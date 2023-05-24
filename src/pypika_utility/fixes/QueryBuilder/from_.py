bf6b8a2954aa419fb43e3667a202f29a552fc746
yugo.omura <yomura1008@gmail.com>
2023-05-24 18:34:32 +0900

    modified:   src/pypika_utility/fixes/QueryBuilder/from_.py

--- src/pypika_utility/fixes/QueryBuilder/from_.py
+++ src/pypika_utility/fixes/QueryBuilder/from_.py
@@ -5,7 +5,7 @@ default_from_method = QueryBuilder.from_
 
 
 def from_(self, selectable):
-    ret = default_from_method(selectable)
+    ret = default_from_method(self, selectable)
     assert len(set(table._table_name for table in self._from)) == len(self._from)
     return ret
 
